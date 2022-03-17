d44cc93404f25319408c73482767a8ad06462eff
not-sruthi <75844345+not-sruthi@users.noreply.github.com>
2022-03-17 18:40:48 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,50 @@
+#program to get song lyrics from azlyrics.com
+
+import requests
+import string
+import string
+from bs4 import BeautifulSoup
+
+special = string.punctuation + ' '
+
+artist = input("Enter the band/artist name: ")
+song = input("Enter the song name: ")
+
+for char in special:
+    artist = artist.replace(char, '')
+    song = song.replace(char, '')
+
+root = "https://www.azlyrics.com/lyrics/"
+
+URL = root + artist.lower() + '/' + song.lower() + '.html'
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+body = soup.find("div", class_="col-xs-12 col-lg-8 text-center")
+
+lyrics = body.text.strip()
+
+fileName = song.lower() +'.txt'
+
+skip = 'Submit Corrections'
+is_skipped = False
+
+with open(fileName, 'w') as file:
+    file.write(URL + '\n \n')
+    for line in lyrics:
+        if 'submit corrections' in line.lower():
+            is_skipped = True
+            
+        else:
+            file.write(line )
+
+with open(fileName,"r+") as f:
+    new_f = f.readlines()
+    f.seek(0)
+    for line in new_f:
+        if "submit correction" not in line.lower():
+            f.write(line)
+        else:
+            break
+    f.truncate()
